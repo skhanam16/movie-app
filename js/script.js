6fe65b81aebce3dc6238e413631990bd3909a6e0
Shaleha Khanam <khanamshaleha@yahoo.com.au>
2023-09-25 16:42:25 +1000

    updated js file

--- js/script.js
+++ js/script.js
@@ -2,5 +2,45 @@
 const global = {
     currentPage: window.location.pathname
 }
+// HIGHLIGHT Active Links
 
+function highlightActiveLink(){
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach((link) =>{
+        if(link.getAttribute('href') === global.currentPage){
+            link.classList.add('active');
+
+        }
+
+    })
+}
+
+// highlightMovie.addEventListener('focus',highlightActiveLink);
+
+// INIT Appp
 console.log(global.currentPage);
+function init(){
+    switch(global.currentPage){
+        case '/':
+        case '/index.html':
+            console.log('Home')
+            break;
+        case '/movie-details.html':
+             console.log('Movie details');
+             break;
+        case '/shows.html':
+            console.log('Shows');
+            break;
+        case '/tv-details.html':
+            console.log('TV details');
+            break;
+        case '/search.html':
+            console.log('Search');
+            break;
+       
+    }
+    highlightActiveLink();
+}
+
+// init();
+document.addEventListener('DOMContentLoaded', init);
