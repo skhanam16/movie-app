8956c0bada68d24c86b4bbb0b6fc4e7672178d60
Shaleha Khanam <khanamshaleha@yahoo.com.au>
2023-09-27 11:30:37 +1000

    updated script file

--- js/script.js
+++ js/script.js
@@ -1,46 +1,88 @@
 // console.log(window.location.pathname);
-const global = {
+// HIghlight links
+let global = {
     currentPage: window.location.pathname
 }
-// HIGHLIGHT Active Links
+// Display popular movies
+async function displayPopularMovies(){
+    const {results} = await fetchAPIData('movie/popular');
+    // console.log(results);
+    results.forEach((movie) =>{
+const div = document.createElement('div');
+div.classList.add('card');
+div.innerHTML = `<a href="movie-details.html?id=${movie.id}">${movie.poster_path ? `<img
+src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+class="card-img-top"
+alt="${movie.title}"
+/>` : ` <img
+src="images/no-image.jpg"
+class="card-img-top"
+alt="${movie.title}"
+/>`}</a>
+<div class="card-body">
+<h5 class="card-title">${movie.title}</h5>
+<p class="card-text">
+  <small class="text-muted">Release: ${movie.release_date}</small>
+</p>
+</div>`;
+const popularMovies = document.getElementById('popular-movies');
+popularMovies.appendChild(div);
+    });
 
-function highlightActiveLink(){
+  
+}
+// console.log("this is "  +  global.currentPage);
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint){
+    const API_KEY = '37deb54b5edb51960465d52d1258a557';
+    const API_URL = 'https://api.themoviedb.org/3/';
+    const res = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+    const data = await res.json();
+    return data;
+    // console.log(data);
+}
+
+
+function highlightedActiveLink(){
     const links = document.querySelectorAll('.nav-link');
     links.forEach((link) =>{
         if(link.getAttribute('href') === global.currentPage){
             link.classList.add('active');
-
         }
-
-    })
+    });
+   
 }
 
-// highlightMovie.addEventListener('focus',highlightActiveLink);
-
-// INIT Appp
-console.log(global.currentPage);
+// init app
 function init(){
+   
     switch(global.currentPage){
         case '/':
-        case '/index.html':
-            console.log('Home')
-            break;
-        case '/movie-details.html':
-             console.log('Movie details');
-             break;
+        case 'index.html':
+        //    console.log('Home');
+           displayPopularMovies();
+           break;
         case '/shows.html':
             console.log('Shows');
             break;
-        case '/tv-details.html':
-            console.log('TV details');
+        case '/movie-details.html':
+            console.log('Movie details');
             break;
         case '/search.html':
             console.log('Search');
             break;
-       
+        case '/tv-details.html':
+            console.log('TV details');
+            break;
+        default:
+            console.log("unknown page");
+
     }
-    highlightActiveLink();
+    highlightedActiveLink();
+    
+
+  
 }
 
-// init();
 document.addEventListener('DOMContentLoaded', init);
